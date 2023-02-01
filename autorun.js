b02cf1569cafc37c757f5899bd037558e1941177
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2023-02-01 12:44:20 +0100

    notification added

--- autorun.js
+++ autorun.js
@@ -6,9 +6,15 @@ Office.initialize = function (reason) { };
 function onNewMessageComposeHandler(event) {
     Office.context.mailbox.item.internetHeaders.setAsync({"x-custom-header": "test"}, function(asyncResult) {
         if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-          console.log("Successfully added custom header");
+            console.log("Successfully added custom header");
+            Office.context.mailbox.item.notificationMessages.replaceAsync("message", {
+                type: Office.MailboxEnums.ItemNotificationMessageType.InsightMessage,
+                actions: [{ actionText: 'Open add-in pane', actionType: 'showTaskPane', commandId: 'paneButton', contextData: '' }],
+                message: 'Successfully added x-custom-header from autorun ',
+                icon: 'icon16'
+            });
         } else {
-          console.log("Error adding custom header: " + JSON.stringify(asyncResult.error));
+            console.log("Error adding custom header: " + JSON.stringify(asyncResult.error));
         }
         event.completed();
       });
